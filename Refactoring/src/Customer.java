81fbcbf8a4a14e8ef99002c4616059bc69b1ee20
Ronei Herter <roneiherter@gmail.com>
2025-11-27 22:40:33 -0300

    Commit 5

--- Refactoring/src/Customer.java
+++ Refactoring/src/Customer.java
@@ -1,30 +1,50 @@
-   public String statement() {
-   double totalAmount = 0;
-   int frequentRenterPoints = 0;
-   Enumeration rentals = _rentals.elements();
-   String result = "Rental Record for " + getname() + "\n";
+import java.util.Enumeration;
+import java.util.Vector;
 
-   while (rentals.hasMoreElements()) {
-      Rental each = (Rental) rentals.nextElement();
+public class Customer {
+   private String _name;
+   private Vector _rentals = new Vector();
 
-      // agora o cálculo está na classe Rental
-      double thisAmount = each.getCharge();
-
-      // add frequent renter points
-      frequentRenterPoints++;
-
-      if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) &&
-           each.getDaysRented() > 1)
-         frequentRenterPoints++;
-
-      result += "\t" + each.getMovie().getTitle() + "\t" +
-            String.valueOf(thisAmount) + "\n";
-
-      totalAmount += thisAmount;
+   public Customer(String name){
+      _name = name;
    }
 
-   result += "Amount owed is " + String.valueOf(totalAmount) + "\n";
-   result += "You earned " + String.valueOf(frequentRenterPoints) +
-           " frequent renter points";
-   return result;
+   public void addRental(Rental arg) {
+      _rentals.addElement(arg);
+   }
+   
+   public String getName(){
+      return _name;
+   }
+
+   public String statement() {
+      double totalAmount = 0;
+      int frequentRenterPoints = 0;
+      Enumeration rentals = _rentals.elements();
+      String result = "Rental Record for " + getName() + "\n";
+
+      while (rentals.hasMoreElements()) {
+         Rental each = (Rental) rentals.nextElement();
+
+         // add frequent renter points
+         frequentRenterPoints++;
+         if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) &&
+             each.getDaysRented() > 1)
+            frequentRenterPoints++;
+
+         // show figures for this rental (thisAmount removido)
+         result += "\t" + each.getMovie().getTitle() + "\t" +
+                   String.valueOf(each.getCharge()) + "\n";
+
+         // totalAmount agora usa getCharge direto
+         totalAmount += each.getCharge();
+      }
+
+      // add footer lines
+      result += "Amount owed is " + String.valueOf(totalAmount) + "\n";
+      result += "You earned " + String.valueOf(frequentRenterPoints) +
+                " frequent renter points";
+
+      return result;
+   }
 }
