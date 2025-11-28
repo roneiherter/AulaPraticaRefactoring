1be721edd53305c4f9bfb34111c5f383d51184ae
Ronei Herter <roneiherter@gmail.com>
2025-11-27 23:06:14 -0300

    Commit 13

--- Refactoring/src/Price.java
+++ Refactoring/src/Price.java
@@ -2,9 +2,11 @@ public abstract class Price {
 
     public abstract int getPriceCode();
 
+    // Agora é abstrato
     public abstract double getCharge(int daysRented);
 
+    // Pode ser sobrescrito por NewReleasePrice
     public int getFrequentRenterPoints(int daysRented) {
-        return 1; // Default
+        return 1;
     }
 }
