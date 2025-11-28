35658f91ac4afa9e142de61214cb64e21e4527a2
Ronei Herter <roneiherter@gmail.com>
2025-11-27 21:19:35 -0300

    Commit 1

--- Refactoring/src/Movie.java
+++ Refactoring/src/Movie.java
@@ -1,5 +1,26 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
+public class Movie {
+
+  public static final int  CHILDRENS = 2;
+  public static final int  REGULAR = 0;
+  public static final int  NEW_RELEASE = 1;
+
+  private String _title;
+  private int _priceCode;
+
+  public Movie(String title, int priceCode) {
+      _title = title;
+      _priceCode = priceCode;
+  }
+
+  public int getPriceCode() {
+      return _priceCode;
+  }
+
+  public void setPriceCode(int arg) {
+      _priceCode = arg;
+  }
+
+  public String getTitle (){
+      return _title;
+  }
 }
